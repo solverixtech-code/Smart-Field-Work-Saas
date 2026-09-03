74bf336ec29518d8e12202377a5328fd0a373bd1
isahibjit <sahibjitsinghramgharia@gmail.com>
2026-09-03 15:06:11 +0530

    feat: initialize API tsconfig and platform modules module structure

--- apps/api/src/platform/modules/platform-modules.module.ts
+++ apps/api/src/platform/modules/platform-modules.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../../auth/auth.module';
 import { PlatformModulesController } from './platform-modules.controller';
 import { PlatformModulesService } from './platform-modules.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PlatformModulesController],
   providers: [PlatformModulesService],
   exports: [PlatformModulesService],
