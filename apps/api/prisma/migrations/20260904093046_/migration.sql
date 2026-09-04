40d8b940320efe9346eb6367b6d8f23ded10b02b
isahibjit <sahibjitsinghramgharia@gmail.com>
2026-09-04 15:00:54 +0530

    chore: remove platformSupport column from ModuleFeature table

---
+++ apps/api/prisma/migrations/20260904093046_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `platformSupport` on the `ModuleFeature` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ModuleFeature" DROP COLUMN "platformSupport";
