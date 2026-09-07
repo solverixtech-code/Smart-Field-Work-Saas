d8acf171bfc25d5fabdb5ce7e142d11491e88004
isahibjit <sahibjitsinghramgharia@gmail.com>
2026-09-07 12:58:49 +0530

    refactor: simplify tenant creation logic by removing unnecessary variable assignment

--- apps/api/src/platform/tenants/tenant.service.ts
+++ apps/api/src/platform/tenants/tenant.service.ts
@@ -176,11 +176,7 @@ export class TenantService {
       await this.tenantRoleService.ensureBuiltInTenantRoles(tenant.id, tx);
 
       return tenant;
-    };
-
-    const createdTenant = transaction
-      ? await create(transaction)
-      : await this.prisma.$transaction(create);
+    });
 
     return this.getTenantById(createdTenant.id, transaction);
   }
